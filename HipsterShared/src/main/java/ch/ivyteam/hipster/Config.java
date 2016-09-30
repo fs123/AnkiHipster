09183c17778c902bea40b43dd449b2667841f565
Reto Weiss <reto.weiss@ivyteam.ch>
2016-09-30 13:12:25 +0200

    Read rabbit-mq host ip from environment variable setuped by kubernetes for service rabbit-mq-msg

--- HipsterShared/src/main/java/ch/ivyteam/hipster/Config.java
+++ HipsterShared/src/main/java/ch/ivyteam/hipster/Config.java
@@ -1,10 +1,18 @@
 package ch.ivyteam.hipster;
 
 public final class Config {
-	public static final String HOST_NAME = "192.168.3.6";
+	public static final String HOST_NAME = getEnvOrDefault("RABBIT_MQ_MSG_SERVICE_HOST", "192.168.3.72");
 	public static final String ANKI_RAW_TOPIC_NAME = "amq.topic";
 
 	public static final String QUEUE_KEY_RAW_MESSAGES = "#";
 	public static final String EXCHANGE_KEY_HIPSTER_MESSAGES = "anki-hipster-message";
+
+	private static String getEnvOrDefault(String envVariableName, String defaultValue) 
+	{
+		String value = System.getenv(envVariableName);
+		value = value != null ? value : defaultValue;
+		System.out.println(value);
+		return value;
+	}
 
 }
